309859976ab370b3b3d9245b31ae7183ef36592c
Anton Mednonogov <mednonogov.anton@gmail.com>
2012-10-24 22:59:30 +0400

    Step-3. Update Task CreatedBy column.
    Disallow null values.

--- step-3/completed/DatabaseScripts/dbo.Task.sql
+++ step-3/completed/DatabaseScripts/dbo.Task.sql
@@ -3,8 +3,8 @@
     [Title]       NVARCHAR (256)  NOT NULL,
     [Description] NVARCHAR (1024) NOT NULL,
     [Completed]   BIT             DEFAULT ((0)) NOT NULL,
-    [CreatedBy] INT NULL, 
-    PRIMARY KEY CLUSTERED ([TaskId] ASC), 
-    CONSTRAINT [FK_CreatedBy] FOREIGN KEY (CreatedBy) REFERENCES [User](UserId)    
+    [CreatedBy]   INT             NOT NULL,
+    PRIMARY KEY CLUSTERED ([TaskId] ASC),
+    CONSTRAINT [FK_CreatedBy] FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[User] ([UserId])
 );
 
