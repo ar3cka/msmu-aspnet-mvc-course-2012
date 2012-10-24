987cc66e19b8488bba5c06bacd404c7f2cba0741
Anton Mednonogov <mednonogov.anton@gmail.com>
2012-10-24 20:12:52 +0400

    Step-3. Add User database table.
    Rename Table.sql file to Task.sql.

--- step-3/completed/DatabaseScripts/dbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].Task
-(
-	[TaskId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Title] NVARCHAR(256) NOT NULL, 
-    [Description] NVARCHAR(1024) NOT NULL, 
-    [Completed] BIT NOT NULL DEFAULT 0
-)
---
+++ step-3/completed/DatabaseScripts/dbo.User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(256) NULL, 
+    [Email] NVARCHAR(256) NOT NULL, 
+    [Password] NVARCHAR(40) NOT NULL
+)
